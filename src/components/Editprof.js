35955e2592e54fff6e3ffcfa02a3981b6721bc8b
Richard <richardngowork@gmail.com>
2022-04-12 00:54:45 -0400

    Added color change fucntionality

--- src/components/Editprof.js
+++ src/components/Editprof.js
@@ -8,13 +8,14 @@ class Edit extends React.Component {
       this.handleChangeOpen = this.handleChangeOpen.bind(this)
       this.handleChangeClose = this.handleChangeClose.bind(this)
       this.handleNameChange = this.handleNameChange.bind(this)
+      this.handleColorChange = this.handleColorChange.bind(this)
       this.state = {
          selected : false,
          userName : "UserName"
         };
     }
 
-  
+
     handleChangeOpen() {
         this.setState({selected : true})
     }
@@ -27,18 +28,41 @@ class Edit extends React.Component {
         console.log(props.target.name.value)
         this.setState({userName : props.target.name.value})
         console.log(this.state.userName)
+        this.handleChangeClose()
     }
+    handleColorChange(props){
+        props.preventDefault();
+        console.log(props.target.color.value)
+        document.body.style.backgroundColor = props.target.color.value
+    }
+    
 
     render() {
       console.log(this.state.selected)
       if(this.state.selected===true){
         return(
             
-            <div>
+            <div >
                 <form onSubmit={this.handleNameChange}>
                     <label>
                         Change Username: 
                         <input type="text" name="name" />
+                    </label>
+                    <button type="submit">Change</button>
+                    
+                </form>
+                <form onSubmit={this.handleColorChange}>
+                    <label>
+                        ChangeBackgroundColor: 
+                        <input type="text" name="color" />
+                    </label>
+                    <button type="submit">Change</button>
+                    
+                </form>
+                <form onSubmit={this.handleColorChange}>
+                    <label>
+                        ChangeBackgroundColor: 
+                        <input type="text" name="color" />
                     </label>
                     <button type="submit">Change</button>
                     
