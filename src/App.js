8027036e1cfb42fc246c5ddd92717d328dbf701b
Richard <richardngowork@gmail.com>
2022-04-11 20:49:57 -0400

    added Clock to main page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Clock from './Clock';
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <Clock />
     </div>
   );
 }
